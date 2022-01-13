0508d6f2aa71ecf4122e5a7f0ec06181d6f25be9
BCMagazine <82822008+BCMagazine@users.noreply.github.com>
2022-01-13 15:14:25 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -229,10 +229,10 @@ function App() {
                 color: "var(--primary-text)",
               }}
             >
-              Transaction Check: 
+              Transaction: 
               
               <StyledLink target={"_blank"} href={CONFIG.SCAN_LINK}>
-                {truncate(CONFIG.CONTRACT_ADDRESS, 25)}
+                {truncate(CONFIG.CONTRACT_ADDRESS, 15)}
               </StyledLink>
             </s.TextDescription>
             <s.SpacerSmall />
